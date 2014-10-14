4d0ba7e197959fffe26a05d4cf8c6bb6491d090f
Jose Antunes <theholy7@gmail.com>
2014-10-14 13:44:47 +0100

    Add: input_validation
    input validation function to show error if input != 2

--- LCS Serial/lcs-serial.c
+++ LCS Serial/lcs-serial.c
@@ -15,13 +15,20 @@ Course:
 #include<stdio.h>
 #include<stdlib.h>
 
+int input_validation (int arg_count, char *arg_vector[]){
 
+  if(arg_count != 2) {
+    printf("\nOnly use one input parameter.\nExample: \"lcs-serial input.in\"\n\n");
+    
+    return -1;
+  }
+  return 1;
+}
 
 /* main: process parameters */
 int main(int argc, char *argv[]) {
 
-
-
+  input_validation(argc, argv);
 
   return 0;
 }
