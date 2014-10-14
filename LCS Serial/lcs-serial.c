e7ec992034b51aabd1caa9578069d3a7dea6fdab
Jose Antunes <theholy7@gmail.com>
2014-10-14 09:23:45 +0100

    Initial Commit: Serial LCS
    Versao serial do projecto

---
+++ LCS Serial/lcs-serial.c
@@ -0,0 +1,27 @@
+/*************************************************************************
+DESCRIPTION:
+        Serial Implementation of LCS algorithm
+
+STUDENTS:
+        César Alves
+        José Antunes
+        Mauro Machado
+
+Course:
+        Computação Paralela e Distribuida - 2014 @ IST
+
+**************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+/* main: process parameters */
+int main(int argc, char *argv[]) {
+
+
+
+
+  return 0;
+}
